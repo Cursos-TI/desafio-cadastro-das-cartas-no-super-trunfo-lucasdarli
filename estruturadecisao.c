1c0fe7bbe37326fc876c6eeada5297589583c678
lucasdarli <lucasdarli@gmail.com>
2025-06-02 08:16:17 -0300

    Update estruturadecisao.c

--- estruturadecisao.c
+++ estruturadecisao.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main (){
 
+  int maca = 76
+  int laranha = 77
+
+if (maca >= laranja);
+  printf ("Tem mais maca do que laranja");
+
+    Else  
+    printf("Tem mais laranha do que maca");
+
+end if
+}
